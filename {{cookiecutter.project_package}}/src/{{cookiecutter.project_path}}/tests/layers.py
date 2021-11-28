f1f2e44086d1b77c0e1670be245080cb8acdf645
Philipp Auersperg-Castell <phil@bluedynamics.com>
2021-11-28 12:05:41 +0100

    setup.py

--- {{cookiecutter.project_package}}/src/{{cookiecutter.project_path}}/tests/layers.py
+++ {{cookiecutter.project_package}}/src/{{cookiecutter.project_path}}/tests/layers.py
@@ -9,12 +9,13 @@ from cone.app.testing import Security
 class {{cookiecutter.project_package.split(".")[-1].capitalize()}}Layer(Security):
 
     def make_app(self):
-        ...
+        kw = {}
+        super().make_app(**kw)
 
     def setUp(self, args=None):
-        ...
+        super().setUp()
 
     def tearDown(self):
-        ...
+        super().tearDown()
 
     
--- {{cookiecutter.project_package}}/src/{{cookiecutter.project_path}}/tests/test_main.py
+++ {{cookiecutter.project_package}}/src/{{cookiecutter.project_path}}/tests/test_main.py
@@ -1,5 +1,10 @@
+from node.tests import NodeTestCase
 from {{cookiecutter.project_package}}.tests.layers import {{cookiecutter.project_package.split(".")[-1].capitalize()}}Layer
 
 
 class TestMain(NodeTestCase):
     layer =  {{cookiecutter.project_package.split(".")[-1].capitalize()}}Layer()
+
+
+    def test_main(self):
+        ...
